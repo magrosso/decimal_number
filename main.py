89b44c5ab2a81d173fd86addafc73ba89455c718
matteo <matteo@delgrosso.de>
2023-08-22 08:25:30 +0200

    extract method

--- main.py
+++ main.py
@@ -1,17 +1,24 @@
-def move_decimal(orig: str, dist: int) -> str:
+def shift_decimal_by(orig: str, dist: int) -> str:
     new_str: str = orig
     try:
         pos: int = new_str.index(".")
     except ValueError:
-        pos = len(orig)
+        pos = len(new_str)
 
+    return move_decimal_to_pos(new_str=new_str, new_pos=pos + dist)
+
+
+def move_decimal_to_pos(new_str: str, new_pos: int) -> str:
     new_str: str = new_str.replace(".", "")
-    new_pos = pos + dist
-
     if new_pos < 0:
         new_str = new_str.zfill(len(new_str) + abs(new_pos))
         return f".{new_str}"
     elif new_pos < len(new_str):
         return f"{new_str[:new_pos]}.{new_str[new_pos:]}"
     else:
-        return new_str[:new_pos]
+        zero_count: int = new_pos - len(new_str)
+        if zero_count > 0:
+            zeros: str = "0" * zero_count
+            return f"{new_str[:new_pos]}{zeros}"
+        else:
+            return new_str[:new_pos]
