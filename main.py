991809c344ea3f27b2a9ddb1ff9905e3e2a6fc5e
matteo <matteo@delgrosso.de>
2023-08-25 09:26:01 +0200

    shift_decimal_by_split

--- main.py
+++ main.py
@@ -22,3 +22,36 @@ def move_decimal_to_pos(new_str: str, new_pos: int) -> str:
             return f"{new_str[:new_pos]}{zeros}"
         else:
             return new_str[:new_pos]
+
+
+def main() -> None:
+    orig_str: str = "123456.789"
+    for shift in range(-len(orig_str), len(orig_str) + 1):
+        new_str: str = shift_decimal_by_split(orig=orig_str, shift=shift)
+        print(f"{orig_str} shifted {shift}: {new_str}")
+
+
+def shift_decimal_by_split(orig: str, shift: int) -> str:
+    orig_split: list[str] = orig.split(".")
+    if len(orig_split) != 2:
+        raise ValueError
+    left, right = orig_split[0], orig_split[1]
+    if shift < 0:
+        dec_pos: int = len(left) + shift
+        if dec_pos < 0:
+            new_left = ""
+            new_right = "0" * abs(dec_pos) + left + right
+        else:
+            new_left: str = left[: len(left) + shift]
+            new_right: str = f"{left[len(left) + shift :]}{right}"
+    elif shift > 0:
+        new_left: str = f"{left}{right[:shift]}"
+        new_right: str = f"{right[shift:]}"
+    else:
+        return orig
+
+    return f"{new_left}.{new_right}"
+
+
+if __name__ == "__main__":
+    main()
